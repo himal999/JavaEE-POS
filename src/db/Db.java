59b26a2124d8a7dfe46f2661ebe43f0b4e98e6a7
himal999 <himal.official99@gmail.com>
2022-05-04 20:49:36 +0530

    create db connection using singalton design pattern

---
+++ src/db/Db.java
@@ -0,0 +1,29 @@
+package db;
+
+/*
+author :Himal
+version : 0.0.1
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Db {
+    static private Db instance;
+    private Connection connection;
+
+    private Db() throws ClassNotFoundException, SQLException {
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root","");
+    }
+
+    public static Db db() throws SQLException, ClassNotFoundException {
+        return  (instance==null) ? instance=new Db() : instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
